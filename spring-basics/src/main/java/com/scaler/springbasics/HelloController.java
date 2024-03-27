18f549a21eacf30695d2a777bfbfa716e161db6e
sabarinathanfb <77018757+sabarinathanfb@users.noreply.github.com>
2024-03-28 03:50:06 +0530

    Spring Basic: Basic get request in controller

---
+++ spring-basics/src/main/java/com/scaler/springbasics/HelloController.java
@@ -0,0 +1,26 @@
+package com.scaler.springbasics;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/world")
+    public String helloworld(){
+
+        return "Hello World!";
+    }
+
+    @GetMapping("/you")
+    public String helloyou(@RequestParam String name){
+
+        return "Hi " +name + "!";
+    }
+
+
+}
