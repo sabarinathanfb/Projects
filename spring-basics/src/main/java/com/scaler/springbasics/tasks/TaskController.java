837def860fab76ebcca603b0a52b25f6b2c68ef9
sabarinathanfb <77018757+sabarinathanfb@users.noreply.github.com>
2024-03-29 00:27:37 +0530

    Adding feature to update taskName and task completed

--- spring-basics/src/main/java/com/scaler/springbasics/tasks/TaskController.java
+++ spring-basics/src/main/java/com/scaler/springbasics/tasks/TaskController.java
@@ -1,6 +1,7 @@
 package com.scaler.springbasics.tasks;
 
 
+import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,16 +15,18 @@ public class TaskController {
 
     List<Task> taskList = new ArrayList<>();
     private int nextTaskId = 1;
-    @GetMapping("")
-    List<Task> getAllTasks(){
-        return taskList;
-    }
+
 
     @PostMapping("")
     Task createTask(@RequestBody Task task){
         task.setId(nextTaskId++);
         taskList.add(task);
         return task;
+    }
+
+    @GetMapping("")
+    List<Task> getAllTasks(){
+        return taskList;
     }
 
     @GetMapping("/{id}")
@@ -34,6 +37,37 @@ public class TaskController {
 
         return foundTask;
     }
+
+
+    @PatchMapping("completed/{taskId}")
+    void markTaskCompleted(@PathVariable Integer taskId) {
+        try {
+            for (Task task : taskList) {
+                if (task.getId().equals(taskId)) {
+                    task.setCompleted(true);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Task ID not found: " + taskId);
+        }
+
+    }
+    @PatchMapping("edit/{taskId}")
+    void editTaskName(@PathVariable Integer taskId, @RequestBody Task updatedTask) {
+        try {
+            for (Task task : taskList) {
+                if (task.getId().equals(taskId)) {
+                    task.setName(updatedTask.getName());
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Task ID not found: " + taskId);
+        }
+
+    }
+
     @DeleteMapping("/delete")
     void deleteTask(@RequestParam Integer taskId){
 
@@ -46,6 +80,7 @@ public class TaskController {
 
     @DeleteMapping("/deleteAll")
     void deleteAllTasks() {
+
         taskList.clear(); // Clears all tasks in the list
     }
 }
